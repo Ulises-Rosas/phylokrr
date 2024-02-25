c5337adecc1bf9ab50bf7b0f997fe32b5c6a7405
Ulises Rosas <ulisesfrosasp@gmail.com>
2024-02-25 01:49:22 -0600

    weighted r2 and rmse

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(name = "phylokrr",
-      version = '0.4.1',
+      version = '0.4.2',
     #   long_description = readme,
     #   long_description_content_type = 'text/markdown',
       packages = ['phylokrr'],
--- src/kernels.py
+++ src/kernels.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from phylokrr.metrics import rmse, R2
 
 def distance_matrix(a, b):
     """
@@ -82,7 +82,7 @@ class KRR:
             covariance matrix
         """
         
-        if vcv is None:
+        if isinstance(vcv, type(None)):
             return None
         
         if self.check_cov:
@@ -151,24 +151,66 @@ class KRR:
         
         return K_test @ self.alpha + self.intercept
     
-    def score(self, X_test, y_test, vcv_test):
+    def score(self, X_test, y_test, vcv_test, metric = 'rmse'):
         """
-        calculates weighted RMSE
+        
+        Calculates score considerint the covariance matrix
+        This error is the same as the in objective function
+
+        X_test: np.array
+            testing data
+        
+        y_test: np.array
+            testing targets
+        
+        vcv_test: np.array
+            testing covariance matrix. If None, then it
+            will return the standard RMSE and R^2
+
+        metric: str, default = 'rmse'
+            weighted root mean squared error (the same 
+            that was used to construct the objective function).
+            if metric = 'r2', then it returns the R^2 which
+            also considers the covariance matrix, see Willett & Singer (1988)
+
+        Returns:
+        --------
+        float
+            score
+
+        Reference:
+        ---------
+        Willett, J. B., & Singer, J. D. (1988). 
+        Another cautionary note about R 2: Its use 
+        in weighted least-squares regression analysis. 
+        The American Statistician, 42(3), 236-238.
         """
 
         y_pred = self.predict(X_test)
         r = y_pred - y_test
-
-        if vcv_test is None:
-            return np.sqrt( np.mean( r**2 ) )
         
-        P = self.P_mat(vcv_test)
-        
-        return np.sqrt( np.mean( (P @ r)**2 ) )
-    
-    def rmse(self, K, alpha, Y):
+        if isinstance(vcv_test, type(None)):
+            P = np.eye(X_test.shape[0])
 
-        return np.sqrt( np.mean( (K.dot(alpha) - Y)**2 ) )
+        else:
+            P = self.P_mat(vcv_test)
+
+        werr = P @ r # weighted residuals
+        Py   = P @ y_test # weighted targets
+
+        if metric == 'rmse':
+            return rmse(werr)
+        
+        else:
+            return R2(Py, werr)
+            
+        # if metric == 'rmse':
+        #     return np.sqrt( np.mean( r**2 ) )
+        
+        # else:
+        #     u = r.T @ r
+        #     v = y_test.T @ y_test - n * np.mean(y_test)**2
+        #     return 1 - (u/v)
     
     def opt_alpha(self, K, y, reg_lam = None, P = None):
         # Y = y
@@ -177,10 +219,9 @@ class KRR:
         n,_ = self.X.shape
         
         I = np.eye(K.shape[0])
-        # I[0,0] = 0
         nlI = n * reg_lam * I
 
-        if P is None:
+        if isinstance(P, type(None)):
             return np.linalg.solve(K + nlI, y)
         
         else:
