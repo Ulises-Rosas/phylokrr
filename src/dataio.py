551558042658059f9d969eba23f037d2f2ff98b8
Ulises Rosas <ulisesfrosasp@gmail.com>
2025-02-16 21:22:43 -0600

    better ordering

--- src/dataio.py
+++ src/dataio.py
@@ -98,24 +98,28 @@ def get_Xy(data_file, verbose = False, delimiter = ',', y_col = -1):
 
     return X, y
 
-def linTime_intersection(names_vcv, spps_ff):
-    # names_vcv, spps_ff
+def linTime_intersection(names_vcv, spps_ff, verbose = False):
+    
+    # O(n)
+    vcv_dict = {names_vcv[i] : i for i in range(len(names_vcv))}
 
+    vcv_ord = deque([])
+    dat_ord = deque([])
     # O(n)
-    a_dict = {spps_ff[i] : i for i in range(len(spps_ff))}
-    # O(n)
-    b_dict = {names_vcv[i] : i for i in range(len(names_vcv))}
+    for i in range(len(spps_ff)):
+        tmp_spps  = spps_ff[i]
 
-    out = deque([])
-    # O(n)
-    for k in b_dict.keys():
-        if not k in a_dict:
+        # all data file rows must 
+        # contain correlation information
+        if not tmp_spps in vcv_dict:
+            if verbose:
+                print(f"{tmp_spps} not found in the tree file")
             continue
 
-        tmp_idx = a_dict[k]
-        out.append(tmp_idx)
+        vcv_ord.append( vcv_dict[tmp_spps] )
+        dat_ord.append( i )
 
-    return list(out)
+    return list(vcv_ord), list(dat_ord)
 
 
 def intersect_w_spps(names_vcv, spps_file, X, y, delimiter = ',', verbose = False):
@@ -144,11 +148,11 @@ def intersect_w_spps(names_vcv, spps_file, X, y, delimiter = ',', verbose = Fals
 
     Returns
     -------
-    X : numpy array
-        ordered predictors
-
-    y : numpy array
-        ordered response variable
+    vcv_ord : list
+        indices of the species names in the tree file
+    
+    dat_ord : list
+        indices of the species names in the data file
     """
 
     # species names from file
@@ -159,17 +163,14 @@ def intersect_w_spps(names_vcv, spps_file, X, y, delimiter = ',', verbose = Fals
 
 
     # match spps_ff and names
-    iter_indx = linTime_intersection(names_vcv, spps_ff)
-    assert len(iter_indx) > 0, "species names must match the tree and data file"
+    vcv_ord, dat_ord = linTime_intersection(names_vcv, spps_ff, verbose = verbose)
+    assert len(vcv_ord) > 0, "species names must match the tree and data file"
 
     # verbose = True
     if verbose:
-        print(f"{len(spps_ff)} names from data, {len(names_vcv)} from tree, {len(iter_indx)} intersect")
+        print(f"{len(spps_ff)} names from data, {len(names_vcv)} from tree, {len(vcv_ord)} intersect")
 
-    X = X[iter_indx,:]
-    y = y[iter_indx]
-
-    return X, y
+    return vcv_ord, dat_ord
 
 def read_data(tree_file, data_file, data_file_spps, y_col = -1, delimiter = ',', verbose = False):
     """
@@ -212,7 +213,12 @@ def read_data(tree_file, data_file, data_file_spps, y_col = -1, delimiter = ',',
         response variable
 
     vcv : numpy array
-        variance-covariance matrix
+        variance-covariance matrix. This matrix is ordered
+        based on the species names in the data file.
+        The item i,j in the matrix represents the correlation 
+        between species i and j and it is obtained by calculating 
+        the distance between the root and the lowest common
+        ancestor of species i and j in the tree.
     """
 
     with open(tree_file, 'r') as f:
@@ -229,17 +235,46 @@ def read_data(tree_file, data_file, data_file_spps, y_col = -1, delimiter = ',',
                   delimiter=delimiter)
     
     
-    X, y = intersect_w_spps(names_vcv, data_file_spps, X, y, 
-                            delimiter=delimiter,
-                            verbose = verbose)
+    vcv_ord, dat_ord = intersect_w_spps(names_vcv, data_file_spps, X, y, 
+                                 delimiter=delimiter,
+                                 verbose = verbose)
+    
+    vcv = vcv[vcv_ord,:][:,vcv_ord]
+
+    X = X[dat_ord,:]
+    y = y[dat_ord]
+
     return X, y, vcv
 
-# Internal original file location: get_Xy_cov.py from LS code base
 
-# tree_file = "./data/test_tree.txt"
+# from phylokrr.utils import weight_data
+
+# tree_file      = "./data/test_tree.txt"
 # data_file_spps = "./data/test_data_spps.txt"
-# from phylokrr.datasets import load_1d_data_example
-# from phylokrr.utils import weight_data, P_inv_simple
+# data_file      = "./data/test_data3.csv"
+
+# X_uw_uc0, y_uw_uc0, vcv0 = read_data(tree_file, data_file, data_file_spps,
+#                                  y_col = 1, delimiter = ',', verbose = True)
+
+# X_w0, y_w0 = weight_data(X_uw_uc0, y_uw_uc0, vcv0, use_sd = False)
+
+# np.random.seed(12038)
+
+# import matplotlib.pyplot as plt
+
+# fig, axs = plt.subplots(1, 2, figsize=(8, 4))
+# axs[0].scatter(X_uw_uc0, y_uw_uc0, color = 'red', alpha=0.5, )
+# axs[0].set_title('Unweighted data')
+# axs[0].set_xlabel('x')
+# axs[0].set_ylabel('y')
+# axs[1].scatter(X_w0, y_w0, color = 'blue', alpha=0.5, )
+# axs[1].set_title('Phylogenetically weighted data')
+# axs[1].set_xlabel('$x^*$')
+# axs[1].set_ylabel('$y^*$')
+# plt.tight_layout()
+
+
+# Internal original file location: get_Xy_cov.py from LS code base
 
 # with open(tree_file, 'r') as f:
 #     tree = f.read().strip()
@@ -284,26 +319,3 @@ def read_data(tree_file, data_file, data_file_spps, y_col = -1, delimiter = ',',
 # # np.savetxt('../src/data/test_data3.csv', Xy, delimiter=',')
 
 
-# data_file = "./data/test_data3.csv"
-# X_uw_uc0, y_uw_uc0, vcv0 = read_data(tree_file, data_file, data_file_spps,
-#                                  y_col = 1, delimiter = ',', verbose = True)
-
-# X_w0, y_w0 = weight_data(X_uw_uc0, y_uw_uc0, vcv0, use_sd = False)
-
-# y_uw_uc[-1]
-
-
-# np.random.seed(12038)
-
-# import matplotlib.pyplot as plt
-
-# fig, axs = plt.subplots(1, 2, figsize=(8, 4))
-# axs[0].scatter(X_uw_uc0, y_uw_uc0, color = 'red', alpha=0.5, )
-# axs[0].set_title('Unweighted data')
-# axs[0].set_xlabel('x')
-# axs[0].set_ylabel('y')
-# axs[1].scatter(X_w0, y_w0, color = 'blue', alpha=0.5, )
-# axs[1].set_title('Phylogenetically weighted data')
-# axs[1].set_xlabel('$x^*$')
-# axs[1].set_ylabel('$y^*$')
-# plt.tight_layout()
